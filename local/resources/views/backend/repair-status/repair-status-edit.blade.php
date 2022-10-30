280a4cae7aff91ec6fc7efb77220adff84658768
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-10-30 19:59:48 +0700

    Add more repair status file

--- local/resources/views/backend/repair-status/repair-status-edit.blade.php
+++ local/resources/views/backend/repair-status/repair-status-edit.blade.php
@@ -32,32 +32,14 @@
             <h5>edit repair status</h5>
         </div>
         <div class="card-block table-border-style">
-            <form method="post" action="{{ route('serial-number.update', $repairstatus->serial_number_id) }}" id="main-repair-status">
+            <form method="post" action="{{ route('repair-status.update', $repairstatus->repair_status_id) }}" id="main-repair-status">
 
                 @csrf
                 @method('PATCH')
                 <div class="col-md-12">
                     <div class="form-group">
-                        <strong>Serial No</strong>
-                        <input type="text" name="serial_number_no" class="form-control">
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <strong>Product Name</strong>
-                        <input type="text" name="serial_number_product_name" class="form-control" >
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <strong>Type Name</strong>
-                        <input type="text" name="serial_number_type_name" class="form-control" >
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <strong>Lot</strong>
-                        <input type="text" name="serial_number_lot" class="form-control" >
+                        <strong>Repair Status Name</strong>
+                        <input type="text" name="repair_status_name" class="form-control" value="{{ $repairstatus->repair_status_name }}">
                     </div>
                 </div>
 
