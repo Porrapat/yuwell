66b66d9ff135515c820906eacf5ae17da32c2ebf
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-11-05 11:38:38 +0700

    Phase 7 Add New Layout to Warranty.

--- local/resources/views/backend/warranty/warranty-print.blade.php
+++ local/resources/views/backend/warranty/warranty-print.blade.php
@@ -2,14 +2,17 @@
 @section('content')
 <style>
     #mytable {
-        font-family: Arial, Helvetica, sans-serif;
+        /* font-family: Arial, Helvetica, sans-serif; */
         border-collapse: collapse;
         width: 100%;
+        font-size:14px;
     }
 
     #mytable td {
-        border: 1px solid #ddd;
+        /* border: 1px solid #ddd; */
         padding: 8px;
+        padding-top: 24px;
+        padding-bottom: 24px;
     }
 
     #mytable th {
@@ -21,6 +24,15 @@
         border: 1px solid #ddd;
         padding: 8px;
     }
+
+    .dotted {
+        border: 2px dotted #000000;
+        border-style: none none dotted;
+        color: #fff;
+        background-color: #fff;
+        margin: 0px;
+    }
+
 </style>
 
 
@@ -35,8 +47,7 @@
                 <div class="page-title">
                     <div class="text-right">
                         <div>
-                            <div>ใบรายงานผลการให้บริการ</div>
-                            <div>เลขที่ {{ $warranty->warranty_id }} </div>
+                            <div>ใบรับประกันสินค้า</div>
                         </div>
                     </div>
                 </div>
@@ -45,70 +56,52 @@
             <div class="content-table">
                 <div class="card-block table-border-style">
                     <table id="mytable">
-                        <thead>
-                            <tr>
-                                <th>Field</th>
-                                <th>Description</th>
-                            </tr>
-                        </thead>
                         <tbody>
                             <tr>
-                                <td>ID</td>
-                                <td>{{ $warranty->warranty_id }}</td>
+                                <td><strong>ชื่อผลิตภัณฑ์</strong></td>
+
+                                <td>{{  $warranty->warranty_product_name }}<hr class='dotted' /></td>
+                                <td><strong>รุ่น</strong></td>
+                                <td>{{  $warranty->warranty_type_name }}<hr class='dotted' /></td>
                             </tr>
                             <tr>
-                                <td>Serial Number</td>
-                                <td>{{  $warranty->warranty_serial_number }}</td>
+
                             </tr>
                             <tr>
-                                <td>Name</td>
-                                <td>{{  $warranty->warranty_name }}</td>
-                            </tr>
-                            <tr>
-                                <td>Surname</td>
-                                <td>{{  $warranty->warranty_surname }}</td>
-                            </tr>
-                            <tr>
-                                <td>Telephone</td>
-                                <td>{{  $warranty->warranty_telephone }}</td>
-                            </tr>
-                            <tr>
-                                <td>Email</td>
-                                <td>{{  $warranty->warranty_email }}</td>
-                            </tr>
-                            <tr>
-                                <td>Address</td>
-                                <td>{{  $warranty->warranty_address }}</td>
-                            </tr>
-                            <tr>
-                                <td>วันที่รับบริการ</td>
-                                <td>{{  $warranty->warranty_service_date }}</td>
-                            </tr>
-                            <tr>
-                                <td>ร้านที่ซื้อ</td>
-                                <td>{{  $warranty->warranty_shop_name }}</td>
-                            </tr>
-                            <tr>
-                                <td>วันที่ซื้อ</td>
-                                <td>{{  $warranty->warranty_buy_date }}</td>
-                            </tr>
-                            <tr>
-                                <td>ชื่อสินค้า</td>
-                                <td>{{  $warranty->warranty_product_name }}</td>
-                            </tr>
-                            <tr>
-                                <td>ประเภทสินค้า</td>
-                                <td>{{  $warranty->warranty_type_name }}</td>
-                            </tr>
-                            <tr>
-                                <td>Lot</td>
-                                <td>{{  $warranty->warranty_lot }}</td>
+                                <td><strong>S/N</strong></td>
+                                <td>{{  $warranty->warranty_serial_number }}<hr class='dotted' /></td>
+                                <td><strong>Lot Number</strong></td>
+                                <td>{{  $warranty->warranty_lot }}<hr class='dotted' /></td>
                             </tr>
 
+                            <tr>
+                                <td><strong>ร้านที่ซื้อ</strong></td>
+                                <td>{{  $warranty->warranty_shop_name }}<hr class='dotted' /></td>
+                            </tr>
+                            <tr>
+                                <td><strong>วันที่รับประกัน</strong></td>
+                                <td>{{  $warranty->warranty_buy_date }}<hr class='dotted' /></td>
+                                <td><strong>วันที่หมดประกัน</strong></td>
+                                <td>{{  $warranty->warranty_buy_date }}<hr class='dotted' /></td>
+                            </tr>
+                            <tr>
+                                <td><strong>ชื่อ - สกุล</strong></td>
+                                <td>{{ $warranty->warranty_name }} {{  $warranty->warranty_surname }}<hr class='dotted' /></td>
+                                <td><strong>เบอร์โทรศัพท์</strong></td>
+                                <td>{{  $warranty->warranty_telephone }}<hr class='dotted' /></td>
+                            </tr>
                         </tbody>
                     </table>
 
                 </div>
+            </div>
+
+            <div class="content-footer">
+            <strong>บริษัท ยูเวล เมดิคอล (ไทยแลนด์) จำกัด</strong>
+            <br/>
+            ที่อยู่ : 33/4 อาคารเดอะไนน์ ทาวเวอร์ แกรนด์ พระราม 9 อาคาร บี ชั้น 28   ถนนพระราม 9 แขวงห้วยขวาง เขตห้วยขวาง กรุงเทพมหานคร 10310
+
+
             </div>
         </div>
     </section>
